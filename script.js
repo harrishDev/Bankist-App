7df69e3ee74202b2e91d016b9f64a49afe13e77a
Harrish <harrishoffical@gmail.com>
2023-07-23 20:56:59 +0530

    added custom user and pin

--- script.js
+++ script.js
@@ -2,31 +2,31 @@
 
 // Data
 const account1 = {
-  owner: 'Jonas Schmedtmann',
+  owner: 'Harrish M',
   movements: [200, 450, -400, 3000, -650, -130, 70, 1300],
   interestRate: 1.2, // %
-  pin: 1111,
+  pin: 7777,
 };
 
 const account2 = {
-  owner: 'Jessica Davis',
+  owner: 'Vishal Raj',
   movements: [5000, 3400, -150, -790, -3210, -1000, 8500, -30],
   interestRate: 1.5,
-  pin: 2222,
+  pin: 9999,
 };
 
 const account3 = {
-  owner: 'Steven Thomas Williams',
+  owner: 'Kishore Kumar',
   movements: [200, -200, 340, -300, -20, 50, 400, -460],
   interestRate: 0.7,
-  pin: 3333,
+  pin: 5555,
 };
 
 const account4 = {
-  owner: 'Sarah Smith',
+  owner: 'Rohan BK',
   movements: [430, 1000, 700, 50, 90],
   interestRate: 1,
-  pin: 4444,
+  pin: 3333,
 };
 
 const accounts = [account1, account2, account3, account4];
