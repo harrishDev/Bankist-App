81bf0a782146bdd6b4f27cc00146238ddca686da
Harrish <harrishoffical@gmail.com>
2023-07-23 21:26:24 +0530

    Fixed Currency sign

--- script.js
+++ script.js
@@ -67,7 +67,7 @@ const displayMovements = function (movements, sort = false) {
     const html = `   
     <div class="movements__row">
     <div class="movements__type movements__type--${type}">${i + 1}${type}</div>
-    <div class="movements__value">${mov}</div>
+    <div class="movements__value">${mov}€</div>
   </div>
   `;
 
